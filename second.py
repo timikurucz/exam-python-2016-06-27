386d651df7ea6ea6c78fea6ed282f04504b22511
Fejes Aniko <oranjje@gmail.com>
2016-06-26 23:08:39 +0200

    more accurate second.py

--- second.py
+++ second.py
@@ -1,7 +1,6 @@
-# Create a function that takes a filename as string parameter,
+# Create a function that takes a filename and a string as parameter,
 # it should write the string to the file 3 times
-# example: "apple" should write "appleappleapple" to the given file
+# example: when called with "tree.txt" and "apple",
+# it should write "appleappleapple" to the file "tree.txt".
 # the function should not raise an error on any output problem, for example
 # denied permission
-
-
